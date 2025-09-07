3ab428ef79c2b4bb136957e956ec113d913a6e1c
Jose Marmolejos <jose.marmolejos@xennialinnovations.com>
2025-09-07 16:14:12 -0400

    Remove unused files

--- apps/api/src/v1/services/auth/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as authRoute } from './auth.route';
-export * as authController from './auth.controller';
-export * as authService from './auth.service';
-export { credentialList } from './auth.model';
-export { Credentials } from './auth.types';
--- apps/api/src/v1/services/todos/todo.model.ts
+++
@@ -1,5 +0,0 @@
-import { Todo } from './todo.types';
-import { TODOS_DATA } from '../../../mocks';
-export const todoList: Todo[] = TODOS_DATA;
-
-//INFO: Add your model here
