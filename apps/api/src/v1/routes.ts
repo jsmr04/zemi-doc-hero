cbf5871bf8a71ac593f329fc16edc0bc6a493dd2
Jose Marmolejos <jose.marmolejos@xennialinnovations.com>
2025-09-08 23:20:07 -0400

    Implement convert service

--- apps/api/src/v1/routes.ts
+++ apps/api/src/v1/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { coreRoute } from './services/core';
 import { fileRoute } from './services/file';
+import { convertRoute } from './services/convert';
 
 export type Route = {
   routeName: string;
@@ -9,6 +10,7 @@ export type Route = {
 const routes = [
   { routeName: '/core', route: coreRoute },
   { routeName: '/file', route: fileRoute },
+  { routeName: '/convert', route: convertRoute },
 ];
 
 export default routes;
---
+++ apps/api/src/v1/services/convert/index.ts
@@ -0,0 +1,1 @@
+export { default as convertRoute } from './convert.route';
